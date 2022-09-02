34f541d7f3f3f62bdff2d47d1ffc334dd4399e24
Kirk Olson <scoutkirkolson@hotmail.com>
2022-09-02 13:36:32 +0200

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,23 @@
 <template>
   <div class="hello">
-    <banner>Joehoeee fijn dat je er bent!!!!</banner>
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <current-time time-zone="Singapore"><span slot="prefix">Singapore time: </span></current-time>
   </div>
 </template>
 
 <script>
-import Components from '@scoutkirkolson/sko-test-vue-npm-library'
-import '@scoutkirkolson/sko-test-vue-npm-library/dist/sko-test-vue-npm-library.css'
+//import Components from '@scoutkirkolson/sko-test-vue-npm-library'
+//import '@scoutkirkolson/sko-test-vue-npm-library/dist/sko-test-vue-npm-library.css'
+import Vue from 'vue';
+import CurrentTime from '@scoutkirkolson/vite-current-time-component'
 
 export default {
   name: 'HelloWorld',
   components: {
-    Components,
+    CurrentTime,
   },
   props: {
     msg: String
-  }
+  },
 }
 </script>
 
